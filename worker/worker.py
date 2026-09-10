8934b4b194a7264e72f9fc6d5b30cd126b2a8603
Vinicius Marinho <viniciusalexsander1@gmail.com>
2026-09-09 22:48:35 -0300

    feat: enhance worker functionality with race service integration

--- worker/worker.py
+++ worker/worker.py
@@ -1,15 +1,42 @@
 import asyncio
 import logging
 
+from api.core.config import get_settings
+from api.domain.ocblacktop_client import OcblacktopClient
+from api.services.race_service import RaceService
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 async def run_worker() -> None:
     logger.info("Worker iniciado")
+    settings = get_settings()
+
+    ocblacktop_client = OcblacktopClient(
+        base_url=settings.ocblacktop_api_base_url,
+        api_key=settings.ocblacktop_api_key,
+    )
+
+    race_service = RaceService(
+        client=ocblacktop_client,
+    )
 
     while True:
         logger.info("Worker executando...")
+
+        try:
+            races = await race_service.list_races()
+
+            if races.data.currentRace:
+                logger.info(f"Corrida em andamento: {races.data.currentRace.name}, no dia {races.data.currentRace.dateStart}")
+            elif races.data.nextRace:
+                logger.info(f"Próxima corrida: {races.data.nextRace.name}, no dia {races.data.nextRace.dateStart}")
+            else:
+                logger.info("Nenhuma corrida em andamento ou próxima encontrada")
+        except Exception as e:
+            logger.error("Erro ao buscar corridas: %s", e)
+
         await asyncio.sleep(60)
 
 
